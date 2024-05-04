701276e5b3bfe4161f2efa3f0ee709ad15fe9c72
Praveen Tiwari <100703294+PraveenTiwari08@users.noreply.github.com>
2024-05-04 21:55:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-//your JS code here. If required.
-function mapLetters(l) {
-    let string = l.toLowerCase(); // Correct method name: toLowerCase()
-    let obj = {};
-    for (let i = 0; i < string.length; i++) {
-        let index = string[i];
-        obj[index] = i;
+function mapLetters(word) {
+    const obj = {};
+    for (let i = 0; i < word.length; i++) {
+        const letter = word[i];
+        if (obj.hasOwnProperty(letter)) {
+            obj[letter].push(i);
+        } else {
+            obj[letter] = [i];
+        }
     }
     return obj;
 }
-
-let l = prompt("Enter the string:");
-alert(JSON.stringify(mapLetters(l))); // Use JSON.stringify() to display the object properly
